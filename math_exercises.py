05d4395b0fcfbdb466c09825c495ce10c1749db7
siim <siim.heinsaar@voco.ee>
2025-09-29 11:30:30 +0300

    Matemaatilised avaldised

---
+++ math_exercises.py
@@ -0,0 +1,28 @@
+import math
+# 1. Sum and difference
+num_a = 3
+num_b = 2
+sum = num_a + num_b
+difference = num_a - num_b
+# 2. Float division
+division = num_a / num_b
+# 3. Integer division
+division2 = num_a // num_b
+# 4. Powerful operations
+multiply_numbers = num_a * num_b
+power =  num_a ** num_b
+remainder = num_a % num_b
+# 5. Find average
+average = (num_a + num_b)/2
+# 6. Area of a circle
+radius = 5
+circle_area = round(math.pi * radius ** 2, 2)
+# 7. Area of an equilateral triangle
+side_lenght = 10
+triangle_area = round((math.sqrt(3) / 4) * (side_lenght ** 2))
+# 8. Calculate discriminant
+discriminant = b ** 2 - 4 * a * c
+# 9
+c = math.sqrt(a ** 2 + b **2)
+# 10
+b = math.sqrt(c ** 2 - a ** 2)

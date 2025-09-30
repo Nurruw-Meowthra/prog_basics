042332b3398ff96ab8b05441238b1103c7c9dea5
siim <siim.heinsaar@voco.ee>
2025-09-30 09:05:00 +0300

    Matemaatilised avaldised

---
+++ math_exercises-jatk.py
@@ -0,0 +1,12 @@
+import math
+# 11. Time converter
+seconds = 1254
+hours = seconds // 3600
+minutes = (seconds % 3600) // 60
+# 12
+radians = math.radians(angle)
+sine = round(math.sin(radians), 1)
+cosine round(math.cos(radians), 1)
+# 13
+n = 5
+lots_of_heys = "Hey" * n
